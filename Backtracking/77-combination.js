b84e21a392743d0be85cda919cb4920ae42d28b9
sanjib-12 <sanjibbarakoti112@gmail.com>
2025-05-27 17:33:45 -0400

    combination

---
+++ Backtracking/77-combination.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+   const result = [];
+
+   const dfs = (start, path) => {
+      if (path.length === k) {
+         result.push([...path]);
+         return;
+      }
+
+      for (let i = start; i <= n; i++) {
+         path.push(i);
+         dfs(i + 1, path);
+         path.pop();
+      }
+   };
+
+   dfs(1, []);
+   return result;
+};
+
+const n = 4,
+   k = 3;
+console.log(combine(n, k));
